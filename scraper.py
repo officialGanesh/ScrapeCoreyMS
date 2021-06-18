993142fb49f8b9ca7ad6f55e2ffd8a40e337f12f
Ganesh <ganuofficial2795@gmail.com>
2021-06-18 11:00:25 +0530

    single page scraped

--- scraper.py
+++ scraper.py
@@ -1,4 +1,6 @@
 
+import csv
+from requests.sessions import session
 from requests_html import HTML
 from aiohttp import ClientSession
 import asyncio
@@ -17,8 +19,9 @@ async def fetch(url,session):
 
         return html_body
 
+
 async def main():
-    '''Scraping the corey's website'''
+    """Scraping the corey's website"""
     
     async with ClientSession() as session:
         
@@ -29,13 +32,58 @@ async def main():
         
         # writing the html in local
         
-        with open('Outputs/index.html','w') as f:
+        with open('Outputs/single/index.html','w') as f:
             f.write(page_content[0].decode())
         
+        # Reading the html in local
+        with open('Outputs/single/index.html','r') as f:
+            source = f.read()
+            html_str = HTML(html=source)
+        
         
 
+        # page check
+        title_box = html_str.find('.title-area',first=True)
+        print(title_box.text)
 
+        # writing data in csv
+        csv_file = open('Outputs\single\data.csv','w',newline='')
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['Title','Published','Author','Project_desc','Video'])
 
+        posts = html_str.find('.post')
+
+        
+        for post in posts:
+            headline = post.find('.entry-title',first=True).text
+            # print(headline)
+
+            date = post.find('.entry-time',first=True).text
+            # print(date)
+
+            author = post.find('.entry-author-name',first=True).text
+            # print(author)
+
+            content_box = post.find('.entry-content',first=True)
+            project_desc = content_box.find('p',first=True).text
+            # print(project_desc)
+
+            # for youtube video links
+            
+            try:
+
+                vid_link = content_box.find('iframe',first=True).attrs['src'].split('?')[0]
+                yt_link = f"https://www.youtube.com/watch?v={vid_link.split('/')[4]}"
+                # print(yt_link)
+
+            except Exception as e:
+                print(url, '😄')
+                # print('Something went wrong ',e)
+
+            csv_writer.writerow([headline,date,author,project_desc,yt_link])
+        csv_file.close()
+
+        
 if __name__ == '__main__':
 
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
